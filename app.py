972e8ce2021945717d4e8a5cdb51e38bfe07ad60
Advali Tech Pty Ltd <bimalkprabha24@gmail.com>
2021-02-19 17:40:32 +0800

    updated Flask
    Update data for Flask App

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask,render_template,request
 from flask.json import jsonify
-app = Flask(__name__)
-
 from sqlalchemy import create_engine
 import pandas as pd
 
-# for pandas reqd_sql
+
+app = Flask(__name__)
+
 engine = create_engine('sqlite:///resources/data.db')
-# connection = engine.connect()
+
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
@@ -20,9 +22,6 @@ def data():
     df = pd.read_sql("SELECT * FROM info",connection)
     records = df.to_json(orient='records')
     return records
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
