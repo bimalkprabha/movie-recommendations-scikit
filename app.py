86616cc639d5f5f00e0f9fce8c54c4d3199cf9c0
Advali Tech Pty Ltd <bimalkprabha24@gmail.com>
2021-02-23 18:28:18 +0800

    updated code

--- app.py
+++ app.py
@@ -64,6 +64,41 @@ def ml(movie_name=str("Child 44")):
         get_title_from_index(movie[0])
     # print(recommnedations[0:15])
     return jsonify(recommnedations[1:15])
+
+@app.route('/ml_1',methods=["POST","GET"])
+def ml_1():
+    movie_name = request.form.get("text")
+    connection = engine.connect()
+    df = pd.read_sql("SELECT * FROM info",connection)
+    features = ['keywords', 'cast', 'genres', 'director']
+
+    for feature in features:
+        df[feature] = df[feature].fillna('')
+    def combined_features(row):
+        return row['keywords']+" "+row['cast']+" "+row['genres']+" "+row['director']
+    df["combined_features"] = df.apply(combined_features, axis =1)
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df["combined_features"])
+    # print("Count Matrix:", count_matrix.toarray())
+    cosine_sim = cosine_similarity(count_matrix)
+    records = df.to_json(orient='records')
+    movie_user_likes = str(movie_name)
+
+    def get_index_from_title(title):
+        value = df.index[df['title'] == title].tolist()
+        return value[0]
+    movie_index = get_index_from_title(movie_user_likes)
+    similar_movies = list(enumerate(cosine_sim[movie_index]))
+# similar_movies
+    sorted_similar_movies = sorted(similar_movies, key=lambda x:x[1], reverse=True)
+# sorted_similar_movies
+    recommnedations =[]
+    def get_title_from_index(index):
+        recommnedations.append(df[df.index == index]["title"].values[0])
+    for movie in sorted_similar_movies:
+        get_title_from_index(movie[0])
+    # print(recommnedations[0:15])
+    return jsonify(recommnedations[1:15])
 # 
 @app.route("/livesearch",methods=["POST","GET"])
 def livesearch():
